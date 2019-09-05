527e091731f8f0e59ce10bbe76669230cb54cdc8
Luis Gaytan <agaytan@techiedodo.com>
2019-09-04 20:10:54 -0700

    routes working

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req,res)=>res.send({msg: 'Welcome to the ContactKeeper API'}));
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
