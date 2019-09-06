4fdf7ac21fbac5675931881aa2b81837bb72e367
Luis Gaytan <agaytan@techiedodo.com>
2019-09-05 23:44:24 -0700

    user model validation, hash password and registration route. Web token with JWT created

--- config/db.js
+++ config/db.js
@@ -3,16 +3,17 @@ const config = require('config');
 
 const db = config.get('mongoURI');
 
-const connectDB = () =>{
-  mongoose.connect(db, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }).then(()=> console.log('MongoDB Connected'))
-  .catch(err=>{
-    console.error(err.message);
-    process.exit(1);
-  });
+const connectDB = async () =>{
+  try {
+    mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log('MongoDB Connected')
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 module.exports = connectDB;
